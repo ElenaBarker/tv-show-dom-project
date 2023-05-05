b527ca9649ab59298985449f179fbf0384c2e809
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-05-05 02:53:17 +0100

    Level 100 done
    added CSS and JS

--- script.js
+++ script.js
@@ -6,7 +6,40 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  for (let i = 0; i < episodeList.length; i++) {
+    
+    
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    rootElem.appendChild(movieCard);
+
+    let episodeTittle = document.createElement("h2");
+    episodeTittle.classList.add("episode-tittle");
+    movieCard.appendChild(episodeTittle);
+
+    let cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+    episodeTittle.appendChild(cardHeader);
+    cardHeader.innerHTML = `${episodeList[i].name} - S0${episodeList[i].season}E0${episodeList[i].number}`;
+
+    let episodeImgContainer = document.createElement("img");
+    episodeImgContainer.classList.add("episode-img-conteiner")
+    movieCard.appendChild(episodeImgContainer);
+    episodeImgContainer.src = episodeList[i].image["medium"];
+
+    let episodeSummary = document.createElement("p");
+    episodeSummary.classList.add("episode-summary");
+    movieCard.appendChild(episodeSummary);
+    episodeSummary.innerHTML = episodeList[i].summary;
+
+
+
+  }
+ 
+  
+ 
 }
 
 window.onload = setup;
