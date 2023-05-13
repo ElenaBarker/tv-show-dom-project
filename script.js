6d66b41c96f24935021e62ce38b08f5698247f8e
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-05-13 17:29:41 +0100

    updates  HTML/JS for level 200

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  rootElem.innerHTML = "";
   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
   for (let i = 0; i < episodeList.length; i++) {
-    
-    
     let movieCard = document.createElement("div");
     movieCard.classList.add("movie-card");
     rootElem.appendChild(movieCard);
@@ -25,7 +24,7 @@ function makePageForEpisodes(episodeList) {
     cardHeader.innerHTML = `${episodeList[i].name} - S0${episodeList[i].season}E0${episodeList[i].number}`;
 
     let episodeImgContainer = document.createElement("img");
-    episodeImgContainer.classList.add("episode-img-conteiner")
+    episodeImgContainer.classList.add("episode-img-conteiner");
     movieCard.appendChild(episodeImgContainer);
     episodeImgContainer.src = episodeList[i].image["medium"];
 
@@ -33,13 +32,33 @@ function makePageForEpisodes(episodeList) {
     episodeSummary.classList.add("episode-summary");
     movieCard.appendChild(episodeSummary);
     episodeSummary.innerHTML = episodeList[i].summary;
-
-
-
   }
- 
-  
- 
+
+  const inputHTML = document.getElementById("search-input");
+  console.log(inputHTML);
+  // inputHTML.episodes = episodeList;
+  inputHTML.addEventListener("keyup", searchMovie);
+
+  function searchMovie(event) {
+    let selectedMovies;
+    let searchContent = event.target.value;
+    // let allEpisodes = event.target.episodes;
+    if (searchContent == "") {
+      setup();
+    } else {
+      let searchContentInsensitive = new RegExp(searchContent, "i");
+      selectedMovies = allEpisodes.filter((episode) => {
+        if (
+          episode.name.match(searchContentInsensitive) !== null ||
+          episode.summary.match(searchContentInsensitive) !== null
+        ) {
+          console.log(episode);
+          return episode;
+        }
+      });
+      makePageForEpisodes(selectedMovies);
+    }
+  }
 }
 
 window.onload = setup;
